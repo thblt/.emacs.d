0fbce23aa463b4410c624867944918df3d9d2bd5
Thibault Polge <thibault@thb.lt>
2017-11-17 13:07:18 +0100

    Cleanup init.el

--- init.el
+++ init.el
@@ -1,59 +1,31 @@
 (setq load-prefer-newer t) ;; Let's do that early.
 
+;;; Borg
+
 (add-to-list 'load-path (expand-file-name "lib/borg" user-emacs-directory))
 (require 'borg)
 (borg-initialize)
 (require 'borg-nix-shell)
 (setq borg-build-shell-command 'borg-nix-shell-build-command)
+(require 'borg-queen)
 
-(require 'no-littering)
-(require 'auto-compile)
+(want-drone ;; borg '(epkg (closql))
+            borg-nix-shell
+            borg-queen)
 
+;;; Auto compile
+
+(want-drone auto-compile)
 (auto-compile-on-load-mode)
-(auto-compile-on-save-mode)
 
-;; A utility macro to declare that a drone is actually required.
-(defvar thblt/required-drones '())
+;;; No littering
 
-(defmacro want-drone (&rest d)
-  "Declare required drones and their dependencies.
+;; We're going to need this at the next step, so it can't be loaded later.
 
-This macro takes a list of symbols D and treats it as a list of
-drone names, optionnally followed by the list of their
-dependencies.  For example:
+(want-drone no-littering)
+(require 'no-littering)
 
-(want-drone borg-queen (borg))
-
-Multiple drones may be declared in a single declaration:
-
-(want-drone drone1 (depA depB)
-            drone2
-            drone3 (depC depB))
-
-For that purpose, this macro is aliased to `want-drones'.
-
-At some point, the Queen will use this to identify orphans.")
-
-(defalias 'want-drones 'want-drone)
-
-(want-drones auto-compile
-             borg
-             no-littering)
-
-;; Before we start, if this is a new installation, build all the
-;; drones.
-
-(let ((flag-file (no-littering-expand-var-file-name "borg/.initial-build-done")))
-  (unless (file-exists-p flag-file)
-    (message "Building Borg drones...")
-    (mapc 'borg-build (borg-drones))
-
-    (make-directory (file-name-directory flag-file))
-    (write-region "" nil flag-file)))
-
-;; We need to configure and (require) borg.el _before_ requiring org
-;; and tangling dotemacs.org, or builtin org-mode will be loaded
-;; instead of Elpa version and updated versions will never be used.
+;;; Load configuration
 
 (unless noninteractive
   (require 'org)
