f8249ba9f84dd269c7d38ba0fc1cbcee93c4af5b
Thibault Polge <thibault@thb.lt>
2018-10-30 19:19:51 +0100

    Massive update and various changes

--- init.el
+++ init.el
@@ -7,10 +7,23 @@
 (borg-initialize)
 (require 'borg-nix-shell)
 (setq borg-build-shell-command 'borg-nix-shell-build-command)
-(defmacro want-drone (&rest _))
+
+(defvar required-drones nil)
+(defun add-required-drone (drone others)
+  (when drone
+    (if (listp drone)
+          (add-required-drone (car drone) (cdr drone))
+          (add-to-list 'required-drones (symbol-name drone))))
+  (when others
+    (add-required-drone (car others) (cdr others)))
+  required-drones)
+
+(defmacro want-drone (drone &rest others)
+  `(add-required-drone (quote ,drone) (quote ,others)))
+
 (defalias 'want-drones 'want-drone)
 
-(want-drone ;; borg '(epkg (closql))
+(want-drone borg (epkg (closql))
             borg-nix-shell
             borg-queen)
 
