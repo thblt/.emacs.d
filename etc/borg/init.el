a77bc9fac7724beac08295f1a0b3177a7135c118
Thibault Polge <thibault@thb.lt>
2019-06-15 18:35:51 +0200

    Fix Borg's init.el

--- etc/borg/init.el
+++ etc/borg/init.el
@@ -2,6 +2,7 @@
 ;; processus fils so this file can be empty.
 
 ;; (add-to-list 'load-path "/home/thblt/.emacs.d/lib/borg-nix-shell")
-;; (require 'borg-nix-shell)
-;; (setq borg-build-shell-command 'borg-nix-shell-build-command
-;;       borg-nix-shell-build-use-pure-shell t)
+;; (require 'borg-nix-shell)A
+(message "Loading Borg user config")
+(setq borg-build-shell-command 'borg-nix-shell-build-command
+      borg-nix-shell-build-use-pure-shell t)
