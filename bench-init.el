5d52b7768de2b24f0e2085309cbe123aa6b6fc8d
Thibault Polge <thibault@thb.lt>
2020-11-01 22:50:16 +0100

    Add quick and dirty benchmarking code

---
+++ bench-init.el
@@ -0,0 +1,17 @@
+(defun bench-buffer (&optional buffer)
+  (interactive)
+  (setq buffer (or buffer (current-buffer)))
+  (with-current-buffer buffer
+    (goto-char (point-min))
+    (remove-overlays (point-min) (point-max))
+    (while (not (eobp))
+      (forward-sexp)
+      (let ((time (current-time))
+	    (overlay (make-overlay (point) (point))))
+	(eval-last-sexp nil)
+	 (overlay-put overlay 'after-string (format " => Spent %4fs" (float-time (time-since time))))))))
+
+
+
+
+(define-key global-map (kbd "<f5>") 'bench-buffer)
