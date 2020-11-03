32d89abe2eb5aaca78059a24b657a4a340db7af4
Thibault Polge <thibault@thb.lt>
2020-11-03 21:06:48 +0100

    Clear blank lines

--- bench-init.el
+++ bench-init.el
@@ -7,11 +7,8 @@
     (while (not (eobp))
       (forward-sexp)
       (let ((time (current-time))
-	    (overlay (make-overlay (point) (point))))
-	(eval-last-sexp nil)
-	 (overlay-put overlay 'after-string (format " => Spent %4fs" (float-time (time-since time))))))))
-
-
-
+	          (overlay (make-overlay (point) (point))))
+	      (eval-last-sexp nil)
+	      (overlay-put overlay 'after-string (format " => Spent %4fs" (float-time (time-since time))))))))
 
 (define-key global-map (kbd "<f5>") 'bench-buffer)
