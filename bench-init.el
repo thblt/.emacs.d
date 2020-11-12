7b7e8d89b8eab728e66e6cf37be9cc2455a3184d
Thibault Polge <thibault@thb.lt>
2020-11-12 16:50:30 +0100

    Improve init benchmarking

--- bench-init.el
+++ bench-init.el
@@ -1,14 +1,29 @@
+(defun cmpcar (a b)
+  "Return non-nil is the car of A is smaller than the car or B."
+  (> (car a) (car b)))
+
 (defun bench-buffer (&optional buffer)
   (interactive)
   (setq buffer (or buffer (current-buffer)))
-  (with-current-buffer buffer
-    (goto-char (point-min))
-    (remove-overlays (point-min) (point-max))
-    (while (not (eobp))
-      (forward-sexp)
-      (let ((time (current-time))
-	          (overlay (make-overlay (point) (point))))
-	      (eval-last-sexp nil)
-	      (overlay-put overlay 'after-string (format " => Spent %4fs" (float-time (time-since time))))))))
+  (let ((benchmark))
+    (with-current-buffer buffer
+      (goto-char (point-min))
+      (remove-overlays (point-min) (point-max))
+      (while (not (eobp))
+        (forward-sexp)
+        (let ((time (current-time)))
+		      (eval-last-sexp nil)
+          (push (cons (float-time (time-since time)) (point)) benchmark)
+          )))
+    ;; Print results
+    (with-current-buffer (get-buffer-create "*Bench*")
+      (org-mode)
+      (insert "| Time | Point |\n")
+      (insert "|------|-------|\n")
+      (mapc (lambda (x)
+              (insert (format"| %4fs | %i |\n" (car x) (cdr x))))
+            (sort benchmark 'cmpcar)
+            )
+      (org-table-align))))
 
 (define-key global-map (kbd "<f5>") 'bench-buffer)
