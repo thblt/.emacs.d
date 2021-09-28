5696ea8ae8e8bca833448fff38ef2fa8aee55aa9
Thibault Polge <thibault@thb.lt>
2021-09-28 17:55:09 +0200

    Make Emacs 28 happy with a regular file as custom-file

--- init.el
+++ init.el
@@ -62,7 +62,7 @@
       create-lockfiles nil
       ;; Don't lose the contents of system clipboard when killing from Emacs:
       save-interprogram-paste-before-kill t
-      custom-file "/dev/null" ;; (no-littering-expand-var-file-name "customize.el")
+      custom-file (no-littering-expand-var-file-name "customize.el")
 
       inhibit-compacting-font-caches (eq system-type 'windows-nt) ; This prevents slowdown when using strange characters.
 
