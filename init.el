c4ee2fa47451a55c925e72789fda0ab220d0f33d
Thibault Polge <thibault@thb.lt>
2023-08-03 15:03:08 +0200

    Require rust-cargo instead of rust-compile in thblt/rust-run

--- init.el
+++ init.el
@@ -1384,7 +1384,7 @@ force target selection, use a prefix argument."
                thblt/rust-run-target)
           (completing-read "Target: " targets)))))
   ;; Save buffers
-  (require 'rust-compile)
+  (require 'rust-cargo)
   (when-let (project-current (project-current))
     (mapc (lambda (buf) (with-current-buffer buf (when (buffer-file-name) (save-buffer))))
           (project-buffers project-current)))
